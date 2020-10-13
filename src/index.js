d4ae01b8661de76a086f2b139405ab2a41492911
zhangchunyu <chunyu.zhang@yuanbaoai.com>
2020-10-13 17:49:38 +0800

    解决使用ant design有严格模式时的警告

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.less';
-import App from './App';
-import { BrowserRouter as Router, Route ,Switch ,Redirect} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.less";
+import App from "./App";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 // react-router 4不支持直接从react-router中直接取hashHistory
 // import { HashRouter as Router, Route, hashHistory } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 // import { createHashHistory } from 'history'
 
 // const hashHistory = createHashHistory();
 
-const routes = (
-    <Router>
-      <Switch>
-        <Redirect exact from="/" to="/home" />
-        {/* <Route path="/user" component={LoginPage} /> */}
-        <Route path="/" component={App} />
-      </Switch>
-    </Router>
-);
-
+// const routes = (
+//   <Router>
+//     <Switch>
+//       <Redirect exact from="/" to="/home" />
+//       {/* <Route path="/user" component={LoginPage} /> */}
+//       <Route path="/" component={App} />
+//     </Switch>
+//   </Router>
+// );
+// 使用ant design时下面代码使用<React.StrictMode>会有警告：
+// findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Wave which is inside 
 ReactDOM.render(
-  <React.StrictMode>
-    {routes}
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  <Router>
+    <Switch>
+      <Redirect exact from="/" to="/home" />
+      {/* <Route path="/user" component={LoginPage} /> */}
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>,
+  // </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
